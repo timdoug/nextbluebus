d26f093c49eaeb9571fee1edba508310647255ec
Tim Douglas <me@timdoug.com>
2013-08-24 18:31:21 -0700

    Make updated date less prominent

--- nextbluebus.py
+++ nextbluebus.py
@@ -144,13 +144,13 @@ PAGE_TEMPLATE = '''<html>
 </head>
 <body><center>
 <h2>When is the next <font color=blue>Blue Bus</font>?</h2>
-Up-to-date as of <b>August 24th, 2013</b>.<br>
 Current time: %s<br>
 <br>
 <table border=1 cellpadding=10>
 <tr><td><font color="red">HC to BMC</font></td><td><font color="blue">BMC to HC</font></td></tr>
 %s</table><br>
 Sweepers in <b>bold</b>.<br>
+Up-to-date as of <b>August 24th, 2013</b>.<br>
 <br>
 The full official schedule is available <a href="http://www.brynmawr.edu/transportation/bico.shtml">here</a>.<br>
 <br>
